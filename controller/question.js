4254048c6efa29e46c8b7090a5e92ca62d1d0aac
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-12 11:55:27 +0300

    Update question.js

--- controller/question.js
+++ controller/question.js
@@ -54,10 +54,41 @@ const deleteQuestion = asyncErrorWrapper(async (req, res, next) => {
         data: question
     })
 })
+const likeQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+    let question = await Question.findById(id);
+    //eğer like edilmişse hata fırlatıyoruz
+    if (question.likes.includes(req.user.id)) {
+        return next(new CustomError("You already liked this question", 400));
+    }
+    question.likes.push(req.user.id);
+    question = await question.save();
+    res.status(200).json({
+        success: true,
+        data: question
+    })
+})
+const unlikeQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+    let question = await Question.findById(id);
+    //eğer like edilmemişse hata fırlatıyoruz
+    if (!question.likes.includes(req.user.id)) {
+        return next(new CustomError("You can not undo operation for this question", 400));
+    }
+    const index = question.likes.indexOf(req.user.id);
+    question.likes.splice(index, 1);
+    question = await question.save();
+    res.status(200).json({
+        success: true,
+        data: question
+    })
+})
 module.exports = {
     askNewQuestion,
     getAllQuestions,
     getSingleQuestion,
     editQuestion,
-    deleteQuestion
+    deleteQuestion,
+    likeQuestion,
+    unlikeQuestion
 }
