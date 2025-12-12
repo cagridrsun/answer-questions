53454a0d6762079bb7ef8afb80a97ce3216056cb
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-12 11:55:24 +0300

    Update question.js

--- routers/question.js
+++ routers/question.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion, deleteQuestion } = require(`../controller/question`);
+const { askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion, deleteQuestion, likeQuestion, unlikeQuestion } = require(`../controller/question`);
 
 const { getAccessToRoute, getQuestionOwnerAccess, getQuestionDeleteAccess } = require(`../middleware/authorization/auth`);
 const { checkQuestionExist } = require(`../middleware/database/databaseErrorHelpers`);
 const router = express.Router();
+router.get("/:id/like", [getAccessToRoute, checkQuestionExist], likeQuestion)
+router.get("/:id/unlike", [getAccessToRoute, checkQuestionExist], unlikeQuestion)
 router.post("/ask", getAccessToRoute, askNewQuestion);
 router.get("/", getAllQuestions);
 router.get("/:id", checkQuestionExist, getSingleQuestion)
