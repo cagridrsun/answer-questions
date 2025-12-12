df3d464ff0420fe632b1d1690c679f1b493cc9cc
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-12 11:55:31 +0300

    Update Question.js

--- models/Question.js
+++ models/Question.js
@@ -25,7 +25,13 @@ const QuestionSchema = new Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: "User"
-    }
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
 
 
 })
