7901ca24c5362bf0ed10218636b985925cc8eaf9
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-15 14:33:26 +0300

    Update answer.js

--- routers/answer.js
+++ routers/answer.js
@@ -1,5 +1,15 @@
 const express = require("express");
 
-const router = express.Router();
-
+const router = express.Router({ mergeParams: true });
+const { getAccessToRoute } = require("../middleware/authorization/auth");
+const { checkAnswerExist } = require("../middleware/database/databaseErrorHelpers");
+const { addNewAnswerToQuestion, getAllAnswers, getSingleAnswer, editAnswer, deleteAnswer, likeAnswer, unlikeAnswer } = require("../controller/answer");
+const { getAnswerOwnerAccess } = require("../middleware/authorization/auth");
+router.post("/", getAccessToRoute, addNewAnswerToQuestion)
+router.get("/", getAllAnswers)
+router.get("/:answer_id", checkAnswerExist, getSingleAnswer)
+router.put("/:answer_id/edit", [checkAnswerExist, getAccessToRoute, getAnswerOwnerAccess], editAnswer)
+router.delete("/:answer_id/delete", [checkAnswerExist, getAccessToRoute, getAnswerOwnerAccess], deleteAnswer)
+router.get("/:answer_id/like", [checkAnswerExist, getAccessToRoute], likeAnswer)
+router.get("/:answer_id/unlike", [checkAnswerExist, getAccessToRoute], unlikeAnswer)
 module.exports = router;
