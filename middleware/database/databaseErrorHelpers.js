51341633969dc0a1f75dd38aacfc05f4a95fd487
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-15 14:33:32 +0300

    Update databaseErrorHelpers.js

--- middleware/database/databaseErrorHelpers.js
+++ middleware/database/databaseErrorHelpers.js
@@ -2,7 +2,7 @@ const User = require("../../models/user")
 const CustomError = require("../errors/customError")
 const asyncErrorWrapper = require("express-async-handler")
 const Question = require("../../models/Question")
-
+const Answer = require("../../models/answer")
 const checkUserExist = asyncErrorWrapper(async (req, res, next) => {
     const { id } = req.params;
     const user = await User.findById(id);
@@ -12,15 +12,24 @@ const checkUserExist = asyncErrorWrapper(async (req, res, next) => {
     next();
 })
 const checkQuestionExist = asyncErrorWrapper(async (req, res, next) => {
-    const { id } = req.params;
-    const question = await Question.findById(id);
+    const question_id = req.params.id || req.params.question_id;
+    const question = await Question.findById(question_id);
     if (!question) {
         return next(new CustomError("Question not found", 404));
     }
     next();
 })
-
+const checkAnswerExist = asyncErrorWrapper(async (req, res, next) => {
+    const answer_id = req.params.id || req.params.answer_id;
+    const question_id = req.params.question_id;
+    const answer = await Answer.findOne({ _id: answer_id, question: question_id });
+    if (!answer) {
+        return next(new CustomError("Answer not found", 404));
+    }
+    next();
+})
 module.exports = {
     checkUserExist,
-    checkQuestionExist
+    checkQuestionExist,
+    checkAnswerExist
 }
