4523118db28c5685f9aa8217fc27cd2e940bcd57
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-16 15:16:27 +0300

    Update question.js

--- routers/question.js
+++ routers/question.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const question = require("../models/Question")
 const { askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion, deleteQuestion, likeQuestion, unlikeQuestion } = require(`../controller/question`);
 const answer = require("./answer")
 const { getAccessToRoute, getQuestionOwnerAccess, getQuestionDeleteAccess } = require(`../middleware/authorization/auth`);
 const { checkQuestionExist } = require(`../middleware/database/databaseErrorHelpers`);
+const { questionQueryMiddleware } = require(`../middleware/query/questionQueryMiddleware`);
 const router = express.Router();
 router.get("/:id/like", [getAccessToRoute, checkQuestionExist], likeQuestion)
 router.get("/:id/unlike", [getAccessToRoute, checkQuestionExist], unlikeQuestion)
 router.post("/ask", getAccessToRoute, askNewQuestion);
-router.get("/", getAllQuestions);
+router.get("/", questionQueryMiddleware(question, { population: { path: "user", select: "name profile_image" } }), getAllQuestions);
 router.get("/:id", checkQuestionExist, getSingleQuestion)
 router.put("/:id/edit", [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], editQuestion)
 router.delete("/:id/delete", [getAccessToRoute, checkQuestionExist, getQuestionDeleteAccess], deleteQuestion)
