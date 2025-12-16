623ffcc0f7729133166b1b8ce7fd25df374de8ed
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-16 15:16:16 +0300

    Create queryMiddlewareHelpers.js

---
+++ middleware/query/queryMiddlewareHelpers.js
@@ -0,0 +1,17 @@
+const searchHelper = (searchKey, query, req) => {
+    if (req.query.search) {
+        const searchObject = {}
+
+        const regex = new RegExp(req.query.search, "i");
+        searchObject[searchKey] = regex;
+        return query.where(searchObject);
+    }
+    return query
+}
+const populateHelper = (population, query) => {
+    return query.populate(population);
+}
+module.exports = {
+    searchHelper,
+    populateHelper
+}
