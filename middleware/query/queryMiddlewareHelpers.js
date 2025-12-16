77b4bca054457eb10079c66989b86f4b3af2c3c0
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-16 16:22:05 +0300

    Update queryMiddlewareHelpers.js

--- middleware/query/queryMiddlewareHelpers.js
+++ middleware/query/queryMiddlewareHelpers.js
@@ -11,7 +11,49 @@ const searchHelper = (searchKey, query, req) => {
 const populateHelper = (population, query) => {
     return query.populate(population);
 }
+const questionSortHelper = (query, req) => {
+    //sort : req.query.sortBy most-answered, most-liked, most-recent
+    const sortKey = req.query.sortBy;
+    if (sortKey === "most-answered") {
+        return query.sort("-answerCount")
+    }
+    if (sortKey === "most-liked") {
+        return query.sort("-likeCount")
+    }
+    return query.sort("-createdAt")
+
+
+}
+const pagginationHelper = async (model, query, req) => {
+    //Pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const pagination = {}
+    const total = await model.countDocuments();
+    if (startIndex > 0) {
+        pagination.previous = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+    return {
+        query: query.skip(startIndex).limit(limit),
+        pagination: pagination
+    }
+
+}
+
 module.exports = {
     searchHelper,
-    populateHelper
+    populateHelper,
+    questionSortHelper,
+    pagginationHelper
 }
