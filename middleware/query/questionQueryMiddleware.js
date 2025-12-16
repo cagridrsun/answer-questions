02bb20a5fcd673eff1b1659b2ddeae66fbe861a8
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-16 16:22:09 +0300

    Update questionQueryMiddleware.js

--- middleware/query/questionQueryMiddleware.js
+++ middleware/query/questionQueryMiddleware.js
@@ -1,13 +1,26 @@
 const asyncErrorWrapper = require("express-async-handler")
-const { searchHelper, populateHelper } = require("./queryMiddlewareHelpers");
+const { searchHelper, populateHelper, questionSortHelper, pagginationHelper } = require("./queryMiddlewareHelpers");
 const questionQueryMiddleware = function (model, options) {
     return asyncErrorWrapper(async function (req, res, next) {
         let query = model.find();
         //search
-        query = searchHelper("search", query, req);
+        query = searchHelper("title", query, req);
         if (options && options.population) {
             query = populateHelper(options.population, query);
         }
+        query = questionSortHelper(query, req);
+        const paginationResult = await pagginationHelper(model, query, req);
+        query = paginationResult.query;
+        const pagination = paginationResult.pagination;
+        const queryResults = await query;
+        res
+            .queryResults = {
+            success: true,
+            count: queryResults.length,
+            pagination: pagination,
+            data: queryResults
+        };
+        next();
 
     })
 }
