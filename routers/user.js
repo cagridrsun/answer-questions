3d2a18bab5c0cb651103a72f8d7856ba4dbf397b
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-16 16:22:14 +0300

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { getSingleUser, getAllUsers } = require("../controller/user.js");
 const { checkUserExist } = require("../middleware/database/databaseErrorHelpers.js");
+const User = require("../models/user.js");
+
+const { userQueryMiddleware } = require("../middleware/query/userQueryMiddleware.js");
 const router = express.Router();
 
-router.get("/", getAllUsers);
+router.get("/", userQueryMiddleware(User), getAllUsers);
 router.get("/:id", checkUserExist, getSingleUser);
 
 
