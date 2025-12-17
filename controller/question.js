ddcdc9b33db0853a8fcd7023ee2ceb07d115acf1
cagridrsun <75903283+cagridrsun@users.noreply.github.com>
2025-12-17 10:06:47 +0300

    Update question.js

--- controller/question.js
+++ controller/question.js
@@ -6,18 +6,12 @@ const getAllQuestions = asyncErrorWrapper(async (req, res, next) => {
     return res.status(200).json(res.queryResults)
 })
 const getSingleQuestion = asyncErrorWrapper(async (req, res, next) => {
-    const { id } = req.params;
-    const question = await Question.findById(id);
-    if (!question) {
-        return next(new CustomError("Question not found", 404));
-    }
     res.status(200).json({
         success: true,
-        data: question
+        data: res.queryResult
     })
 })
 const editQuestion = asyncErrorWrapper(async (req, res, next) => {
-    const { id } = req.params;
     const { title, content } = req.body;
     let question = await Question.findById(id);
     question.title = title;
